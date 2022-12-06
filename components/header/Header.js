6004a6beea4f57d12d540a4107d5259f0c5300ee
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2022-12-06 13:34:22 +0100

    modified header css

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,7 +1,7 @@
 export function Header() {
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6">
-      <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+    <div className="mx-auto px-4 sm:px-6 border-b-2">
+      <div className="flex items-center justify-between border-gray-100 py-6 md:justify-start md:space-x-10">
         <div className="flex justify-start lg:w-0 lg:flex-1">
           <a href="/">
             <div className="flex items-center">
