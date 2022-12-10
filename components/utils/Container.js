77afd81f3f9f765e3676818b05f771291d3640d8
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2022-12-10 15:45:43 +0100

    added utils components: Button, Container, \Logo, NavLink

---
+++ components/utils/Container.js
@@ -0,0 +1,11 @@
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+export function Container({ className, ...props }) {
+  return (
+    <div
+      className={clsx("mx-auto max-w-7xl px-4 sm:px-6 lg:px-8", className)}
+      {...props}
+    />
+  );
+}
