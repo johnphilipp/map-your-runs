ed0252e3d11ca040c082763a789d2f69baec99a6
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2022-12-10 17:20:31 +0100

    modified home

--- components/home/Home.js
+++ components/home/Home.js
@@ -28,28 +28,31 @@ export function Home() {
         </h2>
         <div className="mt-6 overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
           <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-            {features.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
-              >
-                <item.icon
-                  className="mt-2 mr-2 h-10 w-10 flex-shrink-0 text-orange-400"
-                  aria-hidden="true"
-                />
-                <div className="ml-4">
-                  <p className="text-lg md:text-xl font-medium text-gray-900">
-                    {item.name}
-                  </p>
-                  <p className="mt-1 text-md md:text-lg text-gray-500">
-                    {item.description}
-                  </p>
-                </div>
-              </a>
+            {features.map((item, index) => (
+              <div>
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                >
+                  <item.icon
+                    className="mt-2 mr-2 h-10 w-10 flex-shrink-0 text-orange-400"
+                    aria-hidden="true"
+                  />
+                  <div className="ml-4">
+                    <p className="text-lg md:text-xl font-medium text-gray-900">
+                      {item.name}
+                    </p>
+                    <p className="mt-1 text-md md:text-lg text-gray-500">
+                      {item.description}
+                    </p>
+                  </div>
+                </a>
+              </div>
             ))}
           </div>
         </div>
+        <br />
         <br />
       </Container>
     </div>
