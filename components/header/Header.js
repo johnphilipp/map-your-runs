483c515cb563cf06d550c4fe2185a8b7ddf083cf
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2022-12-10 15:47:13 +0100

    made header responsive, added logo

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,48 +1,119 @@
-export function Header() {
-  // Header with project name, home link, and heatmap link
-  return (
-    <div className="mx-auto px-4 sm:px-6 border-b-2">
-      <div className="flex items-center justify-between border-gray-100 py-6 md:justify-start md:space-x-10">
-        <div className="flex justify-start lg:w-0 lg:flex-1">
-          <a href="/">
-            <div className="flex items-center">
-              <img
-                className="h-8 w-auto sm:h-10"
-                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/person-running_1f3c3.png"
-                alt=""
-              />
-              <img
-                className="h-8 w-auto sm:h-10"
-                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/world-map_1f5fa-fe0f.png"
-                alt=""
-              />
-              <span className="ml-4 text-xl text-base font-medium text-gray-900 hover:text-gray-500">
-                MapYourRuns
-              </span>
-            </div>
-          </a>
-        </div>
-        <div className="-my-2 -mr-2 md:hidden"></div>
+import { Fragment } from "react";
+import Link from "next/link";
+import { Popover, Transition } from "@headlessui/react";
+import clsx from "clsx";
+import { Container } from "../utils/Container";
+import { Logo } from "../utils/Logo";
+import { NavLink } from "../utils/NavLink";
 
-        <a
-          href="/"
-          className="text-base font-medium text-gray-500 hover:text-gray-900"
+function MobileNavLink({ href, children }) {
+  return (
+    <Popover.Button as={Link} href={href} className="block w-full p-2">
+      {children}
+    </Popover.Button>
+  );
+}
+
+function MobileNavIcon({ open }) {
+  return (
+    <svg
+      aria-hidden="true"
+      className="h-3.5 w-3.5 overflow-visible stroke-slate-700"
+      fill="none"
+      strokeWidth={2}
+      strokeLinecap="round"
+    >
+      <path
+        d="M0 1H14M0 7H14M0 13H14"
+        className={clsx(
+          "origin-center transition",
+          open && "scale-90 opacity-0"
+        )}
+      />
+      <path
+        d="M2 2L12 12M12 2L2 12"
+        className={clsx(
+          "origin-center transition",
+          !open && "scale-90 opacity-0"
+        )}
+      />
+    </svg>
+  );
+}
+
+function MobileNavigation() {
+  return (
+    <Popover>
+      <Popover.Button
+        className="relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none"
+        aria-label="Toggle Navigation"
+      >
+        {({ open }) => <MobileNavIcon open={open} />}
+      </Popover.Button>
+      <Transition.Root>
+        <Transition.Child
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          Home
-        </a>
-        <a
-          href="/dashboard"
-          className="text-base font-medium text-gray-500 hover:text-gray-900"
+          <Popover.Overlay className="fixed inset-0 bg-slate-300/50" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
         >
-          Dashboard
-        </a>
-        <a
-          href="/heatmap"
-          className="text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          Heatmap
-        </a>
-      </div>
-    </div>
+          <Popover.Panel
+            as="div"
+            className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
+          >
+            <MobileNavLink href="/">Home</MobileNavLink>
+            <MobileNavLink href="/dashboard">Dashboard</MobileNavLink>
+            <MobileNavLink href="/heatmap">Heatmap</MobileNavLink>
+          </Popover.Panel>
+        </Transition.Child>
+      </Transition.Root>
+    </Popover>
+  );
+}
+
+export function Header() {
+  return (
+    <header className="py-10">
+      <Container>
+        <nav className="relative z-50 flex justify-between">
+          <div className="flex items-center md:gap-x-12">
+            <div className="flex items-center gap-2">
+              <Link href="/" aria-label="Home">
+                <Logo className="h-14 w-auto drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]" />
+              </Link>
+              <a
+                href="/"
+                className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-600"
+              >
+                MapYourRun
+              </a>
+            </div>
+            <div className="hidden md:flex md:gap-x-6">
+              <NavLink href="/">Home</NavLink>
+              <NavLink href="/dashboard">Dashboard</NavLink>
+              <NavLink href="/heatmap">Heatmap</NavLink>
+            </div>
+          </div>
+          <div className="-mr-1 md:hidden">
+            <MobileNavigation />
+          </div>
+        </nav>
+        <hr className="mt-4" />
+      </Container>
+    </header>
   );
 }
